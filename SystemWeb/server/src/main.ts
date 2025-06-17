2329c5225c28470a58838f70cf47fa5eb7dec1b0
JosephD0310 <dangnguyenhoang8@gmail.com>
2025-06-17 20:11:31 +0700

    update fe-url

--- SystemWeb/server/src/main.ts
+++ SystemWeb/server/src/main.ts
@@ -6,7 +6,7 @@ import session from 'express-session';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://localhost:5173',
+        origin: 'https://local-center-system.vercel.app',
         credentials: true,
     });
     await app.listen(process.env.PORT ?? 3000);
